b709ac7a8eb4fd7091d15fa69ca68868e0255816
Meysam Pishnamaz <meysampishnamaz@gmail.com>
2024-06-26 16:06:37 +0330

    learning about state management

--- app/src/main/java/com/dante/bullseye/GameScreen.kt
+++ app/src/main/java/com/dante/bullseye/GameScreen.kt
@@ -1,5 +1,6 @@
 package com.dante.bullseye
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,6 +11,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -22,30 +27,58 @@ import com.dante.bullseye.ui.theme.BullseyeTheme
 
 @Composable
 fun GameScreen() {
-    Column(horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-        ,modifier = Modifier
+    var alertIsVisible by remember { mutableStateOf(false)
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center, modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
     ) {
         Spacer(modifier = Modifier.weight(.5f))
-        Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.SpaceEvenly,
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly,
             modifier = Modifier.weight(9f)
         ) {
             Text(stringResource(R.string.instruction_text))
-            Text(stringResource(R.string.target_value), fontSize = 32.sp, fontWeight = FontWeight.Bold)
+            Text(
+                stringResource(R.string.target_value),
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold
+            )
             Row(verticalAlignment = Alignment.CenterVertically) {
-                Text(stringResource(R.string.min_value_text), textAlign = TextAlign.Center, modifier = Modifier.padding(start = 16.dp))
-                Slider(value = 0.5f, valueRange = 0.01f..1f, onValueChange = {}, modifier = Modifier.weight(1f))
-                Text(stringResource(R.string.max_value_text), textAlign = TextAlign.Center, modifier = Modifier.padding(end = 16.dp))
+                Text(
+                    stringResource(R.string.min_value_text),
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(start = 16.dp)
+                )
+                Slider(
+                    value = 0.5f,
+                    valueRange = 0.01f..1f,
+                    onValueChange = {},
+                    modifier = Modifier.weight(1f)
+                )
+                Text(
+                    stringResource(R.string.max_value_text),
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(end = 16.dp)
+                )
             }
-            Button(onClick = {}) {
+            Button(onClick = {
+                alertIsVisible = !alertIsVisible
+                Log.i("debug", alertIsVisible.toString())
+            }) {
                 Text(stringResource(R.string.hit_me_button_text))
             }
         }
         Spacer(modifier = Modifier.weight(.5f))
-
-
+        if (alertIsVisible){
+            ResultDialog(
+                hideDialog = {alertIsVisible = false}
+            )
+        }
     }
 
 }
---
+++ app/src/main/java/com/dante/bullseye/ResultDialog.kt
@@ -0,0 +1,26 @@
+package com.dante.bullseye
+
+import androidx.compose.ui.res.stringResource
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun ResultDialog(
+    hideDialog: () -> Unit, modifier: Modifier = Modifier
+) {
+    AlertDialog(onDismissRequest = {
+        hideDialog()
+    }, confirmButton = {
+        TextButton(onClick = {
+            hideDialog()
+        }) {
+            Text(stringResource(id = R.string.result_dialog_button_text))
+        }
+    }, title = { Text(stringResource(id = R.string.result_dialog_title)) },
+        text = { Text(stringResource(id = R.string.result_dialog_message)) }
+    )
+}
