d9a9209a593d11ed87dc4c134a6565cfe60b1e2c
Meysam Pishnamaz <meysampishnamaz@gmail.com>
2024-07-01 13:26:11 +0330

    make app screen orientation landscape only and full screen.

--- app/src/main/java/com/dante/bullseye/GameScreen.kt
+++ app/src/main/java/com/dante/bullseye/GameScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dante.bullseye.ui.theme.BullseyeTheme
@@ -74,7 +75,7 @@ fun GameScreen() {
 
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, device = Devices.AUTOMOTIVE_1024p, widthDp = 864, heightDp = 432)
 @Composable
 fun GameScreenPreview() {
     BullseyeTheme {
