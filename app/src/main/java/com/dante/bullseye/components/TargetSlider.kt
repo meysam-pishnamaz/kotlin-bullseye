8fdf5f6688ae2b045c40f537a156bd1f3231c25b
Meysam Pishnamaz <meysampishnamaz@gmail.com>
2024-07-02 20:49:10 +0330

    Added slider nub image.

--- app/src/main/java/com/dante/bullseye/components/TargetSlider.kt
+++ app/src/main/java/com/dante/bullseye/components/TargetSlider.kt
@@ -1,23 +1,28 @@
 package com.dante.bullseye.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.dante.bullseye.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TargetSlider(
-    modifier: Modifier=Modifier,
+    modifier: Modifier = Modifier,
     value: Float = 0.5f,
-    valueChanged:(Float)->Unit
-){
+    valueChanged: (Float) -> Unit
+) {
     Row(verticalAlignment = Alignment.CenterVertically, modifier = modifier) {
         Text(
             stringResource(R.string.min_value_text),
@@ -28,7 +33,14 @@ fun TargetSlider(
             value = value,
             valueRange = 0.01f..1f,
             onValueChange = valueChanged,
-            modifier = Modifier.weight(1f)
+            modifier = Modifier.weight(1f),
+            thumb = {
+                Image(
+                    modifier = Modifier.size(100.dp),
+                    painter = painterResource(id = R.drawable.nub),
+                    contentDescription = stringResource(R.string.slider_thumb_desc)
+                )
+            }
         )
         Text(
             stringResource(R.string.max_value_text),
